7e46d863b3bf6cced613414c42d023013a66aa36
aek20 <66400022+aek20@users.noreply.github.com>
2022-05-14 00:10:19 +0300

    landing page ui update

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,27 +1,35 @@
-import React from 'react'
-import { Carousel ,Button}from 'react-bootstrap';
-import landPic from '../../img/main.jpg'
-import './landingPage.css'
-import { Link } from "react-router-dom";
-import mainImg from '../../img/main.jpg'
+import React from "react";
+import "./landingPage.css"
+import background from '../../img/main-removebg-preview.png'
+import play from "../../img/icons8-play-64.png"
+class Landingpage2 extends React.Component {
+    render() {
 
+        return (
+            <div className="landingpagebody">
+                <main>
+                    <div className="main-image">
+                        <img src={background} />
+                    </div>
+                    <div className="main-content">
+                        <h1> chatbot builder </h1>
+                        <p>
+                            With just a few clicks, you can build a chatbot that answers the most common questions without coding knowledge
+                        </p>
+                        <div className="main-buttons">
+                            <a href="#" className=" main-button main-btn ">Get Started</a>
+                            <div className="main-button secondary-button">
+                                <img src={play} />Watch Video
+                            </div>
+                        </div>
+                    </div>
+                </main>
 
+            </div>
 
-export default function LandingPage() {
-    return (
-        <div>
+        );
 
-            <h1 className="rotate-scale-up" >you don't have any chatbot?? create New one 
-        <br />
-                <Link to="/signin">      <Button variant="warning" >CREATE</Button></Link>
-            </h1>
-       
-            
-     
-         
-        </div>
-    )
+    }
+
 }
-// const getFocus=()=>{
-
-// }
+export default Landingpage2;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,24 +1,44 @@
 import React from 'react';
+import '../landingPage/landingPage.css'
+
 // import { Container, Nav, NavDropdown} from 'react-bootstrap'
-import './navbar.css'
-import { logout,useState} from '../../firebase/firebase.js';
+
 const Navbar = () => {
     return (
-        <div>
 
-            <div className="topnav">
-                <a className="active" href="#home">Home</a>
-                <a href="#news">News</a>
-                <a href="mainpage">Contact</a>
-                <a href="/dashboard" >Dashboard</a>
-                <a href="/mainpage" onClick={() =>{
-                    logout()
-                    
-                }}> signout 
-                </a>
-            </div>
-        </div>
+        <header id="navbarstick">
+            <nav className="container">
+                <h1 className="logo1">CHATBOT BUILDERZ</h1>
+                <ul className="primary-nav">
+                    <li className="nav-item">
+                        <a href="#">HOME</a>
+                    </li>
+                    <li className="nav-item ">
+                        <a href="#">DASHBOARD</a>
+                    </li>
+                    <li className="nav-item ">
+                        <a href="#">INBOX</a>
+                    </li>
+                    <li className="nav-item ">
+                        <a href="#">CONTACT</a>
+                    </li>
+                </ul>
+                <ul className="login-nav">
+                    <li className="nav-item">
+                        <a href="#">Log In</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" class="main-btn">Sign Up</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+
+
     );
 }
+
+
 
 export default Navbar;
